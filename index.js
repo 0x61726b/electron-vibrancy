538e01c52e22a2b230387f2924add732ab16a864
huang <2374266244@qq.com>
2018-10-30 15:53:03 +0800

    edit

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-var Vibrancy = require('bindings')('Vibrancy');
+var Vibrancy = require('bindings')('Vibrancy')
 
-module.exports = require('bindings')('Vibrancy');
+module.exports = require('bindings')('Vibrancy')
 
-function AddView(buffer,options) {
-	var viewOptions = {
-		Material: options.Material,
-		Position: { x: options.X, y: options.Y},
-		Size: { width: options.Width, height: options.Height},
-		ResizeMask: options.ResizeMask
-	}
-	return Vibrancy.AddView(buffer,viewOptions);
+function AddView (buffer, options) {
+  var viewOptions = {
+    Material: options.Material,
+    Position: { x: options.X, y: options.Y},
+    Size: { width: options.Width, height: options.Height},
+    BorderRadius: options.borderRadius,
+    ResizeMask: options.ResizeMask
+  }
+  return Vibrancy.AddView(buffer, viewOptions)
 }
 
-function RemoveView(buffer,viewId) {
-	var viewOptions = { ViewId: (viewId) };
-    return Vibrancy.RemoveView(buffer, viewOptions);
+function RemoveView (buffer, viewId) {
+  var viewOptions = { ViewId: (viewId) }
+  return Vibrancy.RemoveView(buffer, viewOptions)
 }
 
-function UpdateView(buffer,options) {
-	var viewOptions = {
-		Material: options.Material,
-		Position: { x: options.X, y: options.Y},
-		Size: { width: options.Width, height: options.Height},
-		ViewId: options.ViewId
-	}
-	return Vibrancy.UpdateView(buffer,viewOptions);
+function UpdateView (buffer, options) {
+  var viewOptions = {
+    Material: options.Material,
+    Position: { x: options.X, y: options.Y},
+    Size: { width: options.Width, height: options.Height},
+    ViewId: options.ViewId
+  }
+  return Vibrancy.UpdateView(buffer, viewOptions)
 }
 
-function DisableVibrancy(buffer) {
-	Vibrancy.SetVibrancy(false,buffer);
+function DisableVibrancy (buffer) {
+  Vibrancy.SetVibrancy(false, buffer)
 }
 
 module.exports = {
-	SetVibrancy: function(window,material) {
-		if(window == null)
-			return -1;
-		
-		var width = window.getSize()[0];
-		var height = window.getSize()[1];
+  SetVibrancy: function (window, material) {
+    if (window == null) { return -1 }
 
-		if(material === null || typeof material === 'undefined')
-			material = 0;
+    var width = window.getSize()[0]
+    var height = window.getSize()[1]
 
-		var resizeMask = 2; //auto resize on both axis
+    if (material === null || typeof material === 'undefined') { material = 0 }
 
-		var viewOptions = {
-			Material: material,
-			Width: width,
-			Height: height,
-			X: 0,
-			Y:0,
-			ResizeMask: resizeMask
-		}
+    var resizeMask = 2 // auto resize on both axis
 
-		return AddView(window.getNativeWindowHandle(),viewOptions);
-	},
-	AddView: function(window,options) {
-		return AddView(window.getNativeWindowHandle(),options);
-	},
-	UpdateView: function(window,options) {
-		return UpdateView(window.getNativeWindowHandle(),options);
-	},
-	RemoveView: function(window,viewId) {
-		return RemoveView(window.getNativeWindowHandle(),viewId);
-	},
-	DisableVibrancy: function(window) {
-		return DisableVibrancy(window.getNativeWindowHandle());
-	}
+    var viewOptions = {
+      Material: material,
+      Width: width,
+      Height: height,
+      X: 0,
+      Y: 0,
+      ResizeMask: resizeMask
+    }
+
+    return AddView(window.getNativeWindowHandle(), viewOptions)
+  },
+  AddView: function (window, options) {
+    return AddView(window.getNativeWindowHandle(), options)
+  },
+  UpdateView: function (window, options) {
+    return UpdateView(window.getNativeWindowHandle(), options)
+  },
+  RemoveView: function (window, viewId) {
+    return RemoveView(window.getNativeWindowHandle(), viewId)
+  },
+  DisableVibrancy: function (window) {
+    return DisableVibrancy(window.getNativeWindowHandle())
+  }
 }
--- src/vibrancy_win.cc
+++ src/vibrancy_win.cc
@@ -106,6 +106,120 @@ namespace Vibrancy {
         }
         return result;
     }
+    bool SetBlurBehinds(HWND hwnd, bool state, v8::Local<v8::Array> options) {
+        bool result = false;
+
+        if (IsWindows10()) {
+            const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
+            if (hModule) {
+                typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND,
+                    WINCOMPATTRDATA*);
+                const pSetWindowCompositionAttribute
+                    SetWindowCompositionAttribute =
+                    (pSetWindowCompositionAttribute)GetProcAddress(
+                        hModule,
+                        "SetWindowCompositionAttribute");
+
+                // Only works on Win10
+                if (SetWindowCompositionAttribute) {
+                    ACCENTPOLICY policy =
+                        { state ? ACCENT_ENABLE_BLURBEHIND
+                            : ACCENT_DISABLE , 0, 0, 0 };
+                    WINCOMPATTRDATA data = {19, &policy, sizeof(ACCENTPOLICY) };
+                    result = SetWindowCompositionAttribute(hwnd, &data);
+                }
+                FreeLibrary(hModule);
+            }
+        } else {
+            HRESULT hr = S_OK;
+
+            struct Option
+            {
+                int vX;
+                int vY;
+                int vWidth;
+                int vHeight;
+                int borderRadius;
+            } opt;
+
+            opt.vX = 0;
+            opt.vY = 0;
+            opt.vWidth = 0;
+            opt.vHeight = 0;
+            opt.borderRadius = 0;
+
+
+
+            v8::Isolate* isolate = v8::Isolate::GetCurrent();
+
+            V8Value vPosition = options->Get(
+                v8::String::NewFromUtf8(isolate, "Position"));
+            V8Value vSize = options->Get(v8::String::NewFromUtf8(isolate, "Size"));
+
+            V8Value vAutoResizeMask = options->Get(
+                v8::String::NewFromUtf8(isolate, "ResizeMask"));
+            V8Value vViewId = options->Get(
+                v8::String::NewFromUtf8(isolate, "ViewId"));
+            V8Value vMaterial = options->Get(
+                v8::String::NewFromUtf8(isolate, "Material"));
+
+            V8Value borderRadius = options->Get(
+                v8::String::NewFromUtf8(isolate, "BorderRadius"));
+
+            if (!borderRadius->IsNull() && borderRadius->IsInt32()) {
+                opt.borderRadius = borderRadius->Int32Value();
+            }
+            
+
+            if (!vSize->IsUndefined() && !vSize->IsNull()) {
+                V8Array vaSize =
+                    v8::Local<v8::Array>::Cast(vSize);
+
+                V8Value vWidth =
+                    vaSize->Get(v8::String::NewFromUtf8(isolate, "width"));
+                V8Value vHeight =
+                    vaSize->Get(v8::String::NewFromUtf8(isolate, "height"));
+
+                if (!vWidth->IsNull() && vWidth->IsInt32())
+                    opt.vWidth = vWidth->Int32Value();
+
+                if (!vHeight->IsNull() && vHeight->IsInt32())
+                    opt.vHeight = vHeight->Int32Value();
+            }
+
+            if (!vPosition->IsUndefined() && !vPosition->IsNull()) {
+                V8Array vaPosition = v8::Local<v8::Array>::Cast(vPosition);
+
+                V8Value vX = vaPosition->Get(v8::String::NewFromUtf8(isolate, "x"));
+                V8Value vY = vaPosition->Get(v8::String::NewFromUtf8(isolate, "y"));
+
+                if (!vX->IsNull() && vX->IsInt32())
+                    opt.vX = vX->Int32Value();
+
+                if (!vY->IsNull() && vY->IsInt32())
+                    opt.vY = vY->Int32Value();
+            }
+
+            // RECT rect;  
+            // GetWindowRect(hwnd, &rect);
+            HRGN hRgn = CreateRoundRectRgn(opt.vX, opt.vY, opt.vWidth - opt.vX, opt.vHeight - opt.vY, opt.borderRadius, opt.borderRadius);
+
+            // Create and populate the Blur Behind structure
+            DWM_BLURBEHIND bb = { 0 };
+
+            // Enable Blur Behind and apply to the entire client area
+            bb.dwFlags = DWM_BB_ENABLE | DWM_BB_BLURREGION;
+            bb.fEnable = state;
+            bb.hRgnBlur = hRgn;
+
+            // Apply Blur Behind
+            hr = DwmEnableBlurBehindWindow(hwnd, &bb);
+            if (SUCCEEDED(hr)) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
     VibrancyHelper::VibrancyHelper() {
     }
@@ -122,7 +236,7 @@ namespace Vibrancy {
         uint32_t handle =
             *reinterpret_cast<uint32_t*>(buffer);
         HWND hwnd = (HWND)handle;
-        return SetBlurBehind(hwnd, true);
+        return SetBlurBehinds(hwnd, true, options);
     }
 
     // These are here so compiling doesnt fail.
